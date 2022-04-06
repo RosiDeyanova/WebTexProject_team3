76ab3c3a4cbc5f3c1e5a04e5fd1dfd13eeb0ce6f
NickyAl <nikitotp@gmail.com>
2022-04-06 18:26:00 +0300

    Desktop version and drawing functionality
    to Desktop version:
    -removed drop down menus
    -added all tools and brushes icons
    -replaced size icon with size slider
    -replaced colors icon with color-palette
    
    to Mobile version:
    -replaced sizes dropdown menu with size slider
    
    Added drawing functionality:
    -can drawing
    -can undo
    -can use new(clears the canvas)
    -can resize brush
    -can change color
    
    TODO:
    -fix dropdown menu clipping outside the page
    -fix dropdown color-palette
    -figure how to make scroll on the canvas without scrolling the whole page
    -add other brushes and tools functionality

--- drawing logic.js
+++ drawing logic.js
@@ -1,33 +1,37 @@
+//the white serface we can draw on
 const canvas = document.getElementById("canvas")
-canvas.width = 300
-canvas.height = 300
+canvas.width = 300  //TODO: figure out how to make the canvas be wider than the page and add the ability to scroll only the canvas
+canvas.height = 300 //TODO: also figure out how to zoom in and out
 
+//initializes the canvas to be all white
 let context = canvas.getContext("2d")
 context.fillStyle = "white"
 context.fillRect(0, 0, canvas.width, canvas.height)
 
+let drawColor = "black" //color used for brush and will be used for other tools
+let drawWidth = "50" //sive of the brush or other tool
 
-let drawColor = "black"
-let drawWidth = "50"
-let isDrawing = false
+let isDrawing = false //a flag that is true when we start drawing
 
-let restoreArray = []
-let index = -1
+let historyArray = [] //history array to keep each step of the drawing used to make undo possible
+let index = -1 //index for the array when its -1 we know its empty
 
 //TOOLS
 
-const clearBtn = document.getElementById("clear-btn")
+const clearBtn = document.getElementById("clear-btn") //for now this is button "New" from "More"
 clearBtn.onclick = clearCanvas
 
+//re-initializes the canvas to be all white with empty history array
 function clearCanvas() {
     context.fillStyle = "white"
     context.clearRect(0, 0, canvas.width, canvas.height)
     context.fillRect(0, 0, canvas.width, canvas.height)
 
-    restoreArray = []
+    historyArray = []
     index = -1
 }
 
+//check if we have items in the history array and if so returns to the previous one returning our drawing to the previous step
 const undoBtn = document.getElementById("undo-btn")
 undoBtn.onclick = function () {
     if(index <= 0) {
@@ -35,16 +39,20 @@ undoBtn.onclick = function () {
     }
     else {
         index--
-        restoreArray.pop()
-        context.putImageData(restoreArray[index], 0, 0)
+        historyArray.pop()
+        context.putImageData(historyArray[index], 0, 0)
     }
 }
 
+//COLORS for mobile
+
+//color input element
 const colorPicker = document.getElementById("color-picker")
 colorPicker.oninput = function () {
     drawColor = colorPicker.value
 }
 
+//each one of basic...Color is used when we choose a color from the color-palette, they do not only pick a color for drawColor but also set the color picker value to the same color
 const basicBlackColor = document.getElementById("black")
 basicBlackColor.onclick = function () {
     changeColor("black")
@@ -101,52 +109,136 @@ basicGreenColor.onclick = function () {
     colorPicker.value = "#008000"
 }
 
+//COLORS for desktop
+
+//color input element
+const colorPickerDesktop = document.getElementById("color-picker-desktop")
+colorPickerDesktop.oninput = function () {
+    drawColor = colorPickerDesktop.value
+}
+
+//each one of desktop...Color is used when we choose a color from the color-palette, they do not only pick a color for drawColor but also set the color picker value to the same color
+const desktopBlackColor = document.getElementById("desktop-black")
+desktopBlackColor.onclick = function () {
+    changeColor("black")
+    colorPickerDesktop.value = "#000000"
+}
+const desktopWhiteColor = document.getElementById("desktop-white")
+desktopWhiteColor.onclick = function () {
+    changeColor("white")
+    colorPickerDesktop.value = "#FFFFFF"
+}
+const desktopGrayColor = document.getElementById("desktop-gray")
+desktopGrayColor.onclick = function () {
+    changeColor("gray")
+    colorPickerDesktop.value = "#888888"
+}
+const desktopRedColor = document.getElementById("desktop-red")
+desktopRedColor.onclick = function () {
+    changeColor("red")
+    colorPickerDesktop.value = "#FF0000"
+}
+const desktopLimeColor = document.getElementById("desktop-lime")
+desktopLimeColor.onclick = function () {
+    changeColor("#00FF00")
+    colorPickerDesktop.value = "#00FF00"
+}
+const desktopBlueColor = document.getElementById("desktop-blue")
+desktopBlueColor.onclick = function () {
+    changeColor("blue")
+    colorPickerDesktop.value = "#0000FF"
+}
+const desktopYellowColor = document.getElementById("desktop-yellow")
+desktopYellowColor.onclick = function () {
+    changeColor("yellow")
+    colorPickerDesktop.value = "#FFFF00"
+}
+const desktopOrangeColor = document.getElementById("desktop-orange")
+desktopOrangeColor.onclick = function () {
+    changeColor("orange")
+    colorPickerDesktop.value = "#FFA500"
+}
+const desktopPurpleColor = document.getElementById("desktop-purple")
+desktopPurpleColor.onclick = function () {
+    changeColor("purple")
+    colorPickerDesktop.value = "#800080"
+}
+const desktopDarkRedColor = document.getElementById("desktop-dark-red")
+desktopDarkRedColor.onclick = function () {
+    changeColor("dark-red")
+    colorPickerDesktop.value = "#8B0000"
+}
+const desktopGreenColor = document.getElementById("desktop-green")
+desktopGreenColor.onclick = function () {
+    changeColor("green")
+    colorPickerDesktop.value = "#008000"
+}
+const desktopDarkBlueColor = document.getElementById("desktop-dark-blue")
+desktopDarkBlueColor.onclick = function () {
+    changeColor("dark-blue")
+    colorPickerDesktop.value = "#00008B"
+}
+
+
+//function to change the drawColor to a specific other color
 function changeColor(colorElement) {
     drawColor = colorElement
 }
 
-const brushSize = document.getElementById("brush-size")
+//SIZE
+
+//for mobile
+const brushSize = document.getElementById("brush-size") //slider input for size
 
 brushSize.oninput = function () {
     drawWidth = brushSize.value
 }
 
+//for desktop
+const desktopBrushSize = document.getElementById("brush-size-desktop") //slider input for size
+
+desktopBrushSize.oninput = function () {
+    drawWidth = desktopBrushSize.value
+}
+
 //DRAWING TO THE CANVAS
-// Prevent scrolling when touching the canvas
-document.body.addEventListener("touchstart", function (e) {
-    if (e.target == canvas) {
-      e.preventDefault();
+
+// Prevent scrolling or refreshing the page when touching the canvas (only for touchscreen)
+document.body.addEventListener("touchstart", function (event) {
+    if (event.target == canvas) {
+      event.preventDefault();
     }
   }, { passive: false });
-  document.body.addEventListener("touchend", function (e) {
-    if (e.target == canvas) {
-      e.preventDefault();
+  document.body.addEventListener("touchend", function (event) {
+    if (event.target == canvas) {
+      event.preventDefault();
     }
   }, { passive: false });
-  document.body.addEventListener("touchmove", function (e) {
-    if (e.target == canvas) {
-      e.preventDefault();
+  document.body.addEventListener("touchmove", function (event) {
+    if (event.target == canvas) {
+      event.preventDefault();
     }
   }, { passive: false });
 
 //drawing logic
+//calls start to draw functions for when we press down the left mouse button or touch the screen
 canvas.addEventListener("mousedown", startMouse, false)
 canvas.addEventListener("touchstart", startTouch, false)
 
+//calls drawing functions when we move the mouse or the finger on the screen
 canvas.addEventListener("mousemove", drawMouse, false)
 canvas.addEventListener("touchmove", drawTouch, false)
 
+//calls a stop drawing function when we release the mouse button or stop touching the screen
 canvas.addEventListener("mouseup", stopMouse, false)
-canvas.addEventListener("mouseout", stopMouse, false)
+canvas.addEventListener("mouseout", stopMouse, false) //also calls stop drawing function if we go outside the screen with the mouse
 canvas.addEventListener("touchend", stopTouch, false)
 
 //for mouse
 function startMouse(event) {
-    clientX = event.touches[0].clientX;
-    clientY = event.touches[0].clientY;
     isDrawing = true
     context.beginPath()
-    context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop) //clientX/Y are mouse coordinates and we offset them to be relative to the canvas
+    context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop) //event.clientX/Y are mouse coordinates and we offset them to be relative to the canvas
 }
 
 function drawMouse(event) {
@@ -165,22 +257,21 @@ function stopMouse(event) {
         context.stroke()
         context.closePath()
         isDrawing = false;
-    }
-
-    if (event.type != "mouseout")  {
-    restoreArray.push(context.getImageData(0, 0, canvas.width, canvas.height))
-    index++
+    
+        //after we are done with this step of the drawing we take the painting so far and push it on the history array
+        historyArray.push(context.getImageData(0, 0, canvas.width, canvas.height))
+        index++
     }
 }
 
 //for touch screen
-let clientX, clientY;
+let clientX, clientY; //seems like for touchscreen there is an array of clientX/Ys so we take only the first one
 function startTouch(event) {
     clientX = event.touches[0].clientX;
     clientY = event.touches[0].clientY;
     isDrawing = true
     context.beginPath()
-    context.moveTo(clientX - canvas.offsetLeft, clientY - canvas.offsetTop) //clientX/Y are mouse coordinates and we offset them to be relative to the canvas
+    context.moveTo(clientX - canvas.offsetLeft, clientY - canvas.offsetTop)
 }
 
 function drawTouch(event) {
@@ -201,11 +292,9 @@ function stopTouch(event) {
         context.stroke()
         context.closePath()
         isDrawing = false;
-        console.log("stop")
-    }
 
-    if (event.type != "mouseout")  {
-    restoreArray.push(context.getImageData(0, 0, canvas.width, canvas.height))
-    index++
+        //after we are done with this step of the drawing we take the painting so far and push it on the history array
+        historyArray.push(context.getImageData(0, 0, canvas.width, canvas.height))
+        index++
     }
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,21 @@
--removed light blue borders
--removed row div
--moved undo and redo to more dropdown menu
--code cleanup (removed thing from the css that didnt change anything, and whitespaces from html)
--made dropdown buttons use the same style class
--changed header and brd display to flex
--added canvas
+to Desktop version:
+-removed drop down menus
+-added all tools and brushes icons
+-replaced size icon with size slider
+-replaced colors icon with color-palette
+
+to Mobile version:
+-replaced sizes dropdown menu with size slider
+
+Added drawing functionality:
+-can drawing
+-can undo
+-can use new(clears the canvas)
+-can resize brush
+-can change color
+
+TODO:
+-fix dropdown menu clipping outside the page
+-fix dropdown color-palette
+-figure how to make scroll on the canvas without scrolling the whole page
+-add other brushes and tools functionality
