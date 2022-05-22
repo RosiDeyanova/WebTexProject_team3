a6866afb00bae93ab181c9b6ecd31cc8fb9e1fe5
mariayaneva <maria04@abv.bg>
2022-05-22 14:37:03 +0300

    fixed server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+//import { MongoClient } from 'mongodb';
 const express = require('express');
 
 const app = express();
@@ -7,17 +7,20 @@ app.use(express.json());
 // visualise html
 app.get('/',function(req,res) 
 {
-    res.sendFile('C:/Users/Mimi/Documents/GitHub/WebTexProject_team3/mainPage.html');
+    res.sendFile('C:/Users/Mimi/Documents/GitHub/WebTexProject_team3/public/mainPage.html');
 });
 
 // visualise css (public\css) and js
 app.use(express.static(__dirname + '/'));
 
+/*
 MongoClient.connect("mongodb+srv://root:root@cluster0.8eazs.mongodb.net/?retryWrites=true&w=majority",(error,client)=>{
     if(error){
         client.close();
         return;
     }
 });
+*/
 
 app.listen(3000);
+
