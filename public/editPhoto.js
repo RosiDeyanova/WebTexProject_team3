49226b1eb624becb0ceb45ac68fc45719b0f5752
mariayaneva <maria04@abv.bg>
2022-06-17 16:17:34 +0300

    comments added

--- public/editPhoto.js
+++ public/editPhoto.js
@@ -1,15 +1,15 @@
 const canvas = document.getElementById('canvas'); 
 const context = canvas.getContext('2d'); 
-const img = new Image(); 
+const img = document.createElement('img'); 
 
 var classPics = document.getElementsByClassName("pictures"); // all pictures in album
 
 for (var i = 0; i < classPics.length; i++)
 {
-    // [0] - first pic, [1] - second pic...
+    // [0] - first picture, [1] - second picture...
     if(classPics[i]) // not null check
     {
-        classPics[i].addEventListener("click", bindClick(i));
+        classPics[i].addEventListener("click", bindClick(i)); // if picture is chosen
     }
 }
 
@@ -17,12 +17,11 @@ function bindClick(i)
 {
     return function()
     {
-        img.src = classPics[i].src;        
+        img.src = classPics[i].src; // url of new picture is the url of the clicked picture (style.backgroundImage)
         img.onload = () => 
         {    
-            context.drawImage(img, 0, 0);
+            context.drawImage(img, 0, 0); // put image on canvas with coordinates 0,0
         };
-        //TODO: auto clear canvas after each chosen pic?
     }
 }
 
--- public/uploadPhoto.js
+++ public/uploadPhoto.js
@@ -2,19 +2,20 @@ const image_input = document.getElementById("image-input");
 
 image_input.addEventListener("change", function() 
 {
-  const reader = new FileReader(); //
+  const reader = new FileReader(); // app reads the contents of file
   reader.addEventListener("load", () => 
   {
     // show picture in the rectangle
     const uploaded_image = reader.result;
-        $.post('http://localhost:3000/saveToDb', {
+        $.post('http://localhost:3000/saveToDb', 
+        {
             img: uploaded_image
         });
     var url =  `url(${uploaded_image})`;
     document.getElementById("display-image").style.backgroundImage = url;
     
     // add uploaded picture to album
-    var newPic = document.createElement("div");
+    var newPic = document.createElement("img");
     newPic.classList.add("pictures");
     newPic.style.width = "300px";
     newPic.style.height = "225px";
@@ -24,6 +25,5 @@ image_input.addEventListener("change", function()
     document.getElementById("album-pictures").appendChild(newPic);
   });
   
-  reader.readAsDataURL(this.files[0]); //
-
-})
+  reader.readAsDataURL(this.files[0]); 
+});
